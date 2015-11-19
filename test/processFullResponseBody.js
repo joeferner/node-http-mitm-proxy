72ec898d9aa285cb64746ac18aef3285bbe62589
Félicien François <felicien@tweakstyle.com>
2015-11-19 11:10:46 +0100

    added more test/example use cases

---
+++ test/processFullResponseBody.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var port = 8081;
+
+var Proxy = require('../');
+var proxy = Proxy();
+
+proxy.onError(function(ctx, err) {
+  console.error('proxy error:', err);
+});
+
+proxy.use(Proxy.gunzip);
+
+proxy.onRequest(function(ctx, callback) {
+  var chunks = [];
+  ctx.onResponseData(function(ctx, chunk, callback) {
+    chunks.push(chunk);
+    return callback(null, null); // don't write chunks to client response
+  });
+  ctx.onResponseEnd(function(ctx, callback) {
+    var body = Buffer.concat(chunks);
+    if(ctx.serverToProxyResponse.headers["content-type"] && ctx.serverToProxyResponse.headers["content-type"].indexOf("text/html") === 0) {
+      body = body.toString().replace(/Lucky/g, "Sexy");
+    }
+    ctx.proxyToClientResponse.write(body);
+    return callback();
+  });
+  callback();
+});
+
+
+
+proxy.listen({ port: port });
+console.log('listening on ' + port);
