b901de9d274d115fad4d880e55df2e36aa20a047
JonLuca De Caro <jonluca.decaro@gmail.com>
2022-06-29 15:15:03 -0500

    add dist to gitignore, rebase commits, convert code to es6, convert to typescript (#259)

---
+++ bin/mitm-proxy.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node --experimental-specifier-resolution=node
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+import d from "debug";
+const debug = d("http-mitm-proxy:bin");
+import Proxy from "../lib/proxy";
+const proxy = new Proxy();
+
+const args = yargs(hideBin(process.argv))
+  .alias("h", "help")
+  .alias("h", "?")
+  .options("port", {
+    default: 80,
+    describe: "HTTP Port.",
+  })
+  .alias("p", "port")
+  .options("host", {
+    describe: "HTTP Listen Interface.",
+  }).argv;
+
+if (args.help) {
+  yargs.showHelp();
+  process.exit(-1);
+}
+
+proxy.onError((ctx, err, errorKind) => {
+  debug(errorKind, err);
+});
+proxy.listen(args, (err) => {
+  if (err) {
+    debug(`Failed to start listening on port ${args.port}`, err);
+    process.exit(1);
+  }
+  debug(`proxy listening on ${args.port}`);
+});
---
+++ lib/middleware/gunzip.ts
@@ -0,0 +1,19 @@
+import zlib from "zlib";
+
+export default {
+  onResponse: function (ctx, callback) {
+    if (
+      ctx.serverToProxyResponse.headers["content-encoding"] &&
+      ctx.serverToProxyResponse.headers["content-encoding"].toLowerCase() ==
+        "gzip"
+    ) {
+      delete ctx.serverToProxyResponse.headers["content-encoding"];
+      ctx.addResponseFilter(zlib.createGunzip());
+    }
+    return callback();
+  },
+  onRequest: function (ctx, callback) {
+    ctx.proxyToServerRequestOptions.headers["accept-encoding"] = "gzip";
+    return callback();
+  },
+};
